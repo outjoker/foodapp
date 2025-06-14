60eed067ab2dbb23a027851099f39ba1bb03594d
Bala Srinivas Vanapalli <Bala.Srinivas.Vanapalli@kyndryl.com>
2025-06-14 13:49:02 -0500

    styled the food item list

--- recipeapp/src/components/Fooditem.jsx
+++ recipeapp/src/components/Fooditem.jsx
@@ -1,9 +1,14 @@
+import styles from "./fooditem.module.css"
 export default function Fooditem(props){
     return (
-        <div>
-            <img src={props.eachFoodItem.image}></img>
-            <h1>{props.eachFoodItem.title}</h1>
-            <button>View Recipe</button>
+        <div className={styles.foodItemContainer}>
+            <img className={styles.foodItemImage}
+            src={props.eachFoodItem.image}></img>
+            <h1 className={styles.foodItemTitle}>{props.eachFoodItem.title}</h1>
+            <div className={styles.buttonContainer}>
+                <button className={styles.foodItemButton}>View Recipe</button>
+            </div>
+            
         </div>
     )
 }
