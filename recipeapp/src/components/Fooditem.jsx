8b7299400b159eff583c8c1a1804ae0a5a8f2e9e
V.Balasrinivas <balasrinivas12@gmail.com>
2025-06-14 13:54:33 -0500

    Revert "Navbar (#4)"
    This reverts commit 663fb601fc046b554615a92258f9043df46a5c80.

--- recipeapp/src/components/Fooditem.jsx
+++ recipeapp/src/components/Fooditem.jsx
@@ -1,14 +1,9 @@
-import styles from "./fooditem.module.css"
 export default function Fooditem(props){
     return (
-        <div className={styles.foodItemContainer}>
-            <img className={styles.foodItemImage}
-            src={props.eachFoodItem.image}></img>
-            <h1 className={styles.foodItemTitle}>{props.eachFoodItem.title}</h1>
-            <div className={styles.buttonContainer}>
-                <button className={styles.foodItemButton}>View Recipe</button>
-            </div>
-            
+        <div>
+            <img src={props.eachFoodItem.image}></img>
+            <h1>{props.eachFoodItem.title}</h1>
+            <button>View Recipe</button>
         </div>
     )
 }
